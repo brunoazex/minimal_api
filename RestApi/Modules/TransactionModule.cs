f42d3803a7e7768be97169efe8f406fff34ecc0d
Bruno Azevedo <bcbazevedo@gmail.com>
2022-05-12 17:42:40 -0300

    feature: move endpoints to carter module

---
+++ RestApi/Modules/TransactionModule.cs
@@ -0,0 +1,45 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Mvc;
+using RestApi.Models;
+using RestApi.Services;
+
+namespace RestApi.Modules
+{
+    public class TransactionModule : ICarterModule
+    {
+        private readonly AccountService _accountService;
+
+        public TransactionModule(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/balance", HandleBalance);
+            app.MapPost("/event", HandleEvent);
+            app.MapPost("/reset", HandleReset);
+        }
+
+
+        public IResult HandleBalance(
+            [FromQuery(Name = "account_id")]
+            string accountId)
+        {
+            return Results.Ok(_accountService.GetBalance(accountId));
+        }
+
+        public IResult HandleEvent(
+            NewEvent request
+        )
+        {
+            return Results.Created("balance", _accountService.MakeOperation(request));
+        }
+
+        public IResult HandleReset()
+        {
+            _accountService.Reset();
+            return Results.Content("OK");
+        }
+    }
+}
